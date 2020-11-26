45c1655820392ac71cf44747e7ad232d815f03db
Peter Atkinson <peterjatkinson@gmail.com>
2020-11-26 17:09:55 +0000

    End of 6.1

---
+++ functional.js
@@ -0,0 +1,30 @@
+let states = ["Kansas", "Nebraska", "North Dakota", "South Dakota"];
+
+// Returns a URL-friendly version of a string.
+//   Example: "North Dakota" -> "north-dakota"
+function urlify(string) {
+  return 'https://example.com/' + string.toLowerCase().split(/\s+/).join("-");
+}
+
+// urls: Imperative version
+function imperativeUrls(elements) {
+  let urls = [];
+  elements.forEach(function(element) {
+    urls.push(urlify(element));
+  });
+  return urls;
+}
+console.log(imperativeUrls(states));
+
+// urls: Functional version
+function functionalUrls(elements) {
+  return elements.map(element => urlify(element));
+}
+console.log(functionalUrls(states));
+
+// function that takes in the states variable and returns an array of URLs of the form https://example.com/<urlified form>
+
+function statesToUrl(elements) {
+  return elements.map(element => urlify(element));
+}
+console.log(statesToUrl(states));
