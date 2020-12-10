0d140e7f577b9ee9a457e83f3105ae12cbe1fd35
Peter Atkinson <peterjatkinson@gmail.com>
2020-12-10 17:05:38 +0000

    Almost end of Ch. 7

--- palindrome.js
+++ palindrome.js
@@ -1,20 +1,38 @@
-// Reverses a string.
-function reverse(string) {
-  return Array.from(string).reverse().join("");
+// Adds `reverse` to all strings.
+String.prototype.reverse = function() {
+  return Array.from(this).reverse().join("");
 }
+
+// Add a blank method to the String object prototype that returns true if the string is empty or consists solely of whitespace (spaces, tabs, or newlines)
+String.prototype.blank = function () {
+  return !!(this.match(/^\s*$/g));
+}
+
 
 // Defines a Phrase object.
 function Phrase(content) {
   this.content = content;
 
-  // Returns true if the phrase is a palindrome, false otherwise. This is a method. A method is a property bound to a function
+  // Returns content processed for palindrome testing.
+  this.processedContent = function processedContent() {
+    return this.content.toLowerCase();
+  }
+
+  // Returns true if the phrase is a palindrome, false otherwise.
   this.palindrome = function palindrome() {
-    let processedContent = this.content.toLowerCase();
-    return processedContent === reverse(processedContent);
+    return this.processedContent() === this.processedContent().reverse();
   }
-  // Makes the phrase LOUDER.
-  this.louder = function() {
-    return this.content.toUpperCase();
-  }
-  
 }
+
+// // Defines a TranslatedPhrase object.
+// function TranslatedPhrase(content, translation) {
+//   this.content = content;
+//   this.translation = translation;
+
+//   // Returns translation processed for palindrome testing.
+//   this.processedContent = function processedContent() {
+//     return this.processor(this.translation);
+//   }
+// }
+
+// TranslatedPhrase.prototype = new Phrase();
