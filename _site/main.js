3e356f281a7b2a99ba50f1d127fac5ac9016b979
Peter Atkinson <peterjatkinson@gmail.com>
2021-03-15 09:55:16 +0000

    Changes for testing new remote URL

--- _site/main.js
+++ _site/main.js
@@ -1,12 +1,20 @@
 let Phrase = require("peterjatkinson_palindrome");
 
-let string = prompt("Please enter a string for palindrome testing:");
+function palindromeTester(event) {
+  event.preventDefault();
+  let phrase = new Phrase(event.target.phrase.value);
+  let palindromeResult = document.querySelector("#palindromeResult");
 
-let phrase = new Phrase(string);
+  if (phrase.palindrome()) {
+    palindromeResult.innerHTML = `"${phrase.content}" is a palindrome!`;
+  } else {
+    palindromeResult.innerHTML = `"${phrase.content}" is not a palindrome.`;
+  }
+}
 
-if (phrase.palindrome()) {
-  alert(`"${phrase.content}" is a palindrome!`);
-}
-else {
-  alert(`"${phrase.content}" is not a palindrome.`)
-}
+document.addEventListener("DOMContentLoaded", function() {
+  let tester = document.querySelector("#palindromeTester");
+  tester.addEventListener("submit", function(event) {
+    palindromeTester(event);
+  });
+});
